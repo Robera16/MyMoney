daba3a9ac59098170e61a3915e1638c8270e7089
robera16 <roberaworknehrobwork@gmail.com>
2022-03-04 11:23:04 +0300

    dispatch login action

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -7,10 +7,10 @@ export default function Navbar() {
   return (
     <nav className={styles.navbar}>
         <ul >
-            <li className={styles.title}>myMoney</li>
+            <li className={styles.title}><Link to="/">myMoney</Link></li>
             
             <li><Link to="/login">Login</Link></li>
-            <li><Link to="/login">Signup</Link></li>
+            <li><Link to="/signup">Signup</Link></li>
         </ul>
     </nav>
   )
--- src/hooks/useSignup.js
+++ src/hooks/useSignup.js
@@ -1,9 +1,11 @@
 import { useState } from "react"
 import { projectAuth } from "../firebase/config"
+import { useAuthContext } from "./useAuthContext"
 
 export const useSignup = () => {
     const[error, setError] = useState(null)
     const[isPending, setIsPending] = useState(false)
+    const { dispatch } = useAuthContext()
 
     const signup = async (email, password, displayName) => {
         // reset the error every time we try to signup
@@ -13,7 +15,6 @@ export const useSignup = () => {
         try {
             // signup user
             const res = await projectAuth.createUserWithEmailAndPassword(email, password)
-            console.log(res.user)
 
             if(!res){ // bad connection
                 throw new Error('Could not complete signup')
@@ -21,6 +22,9 @@ export const useSignup = () => {
 
             // add display name to user
             await res.user.updateProfile({ displayName: displayName})
+
+            // dispatch login action
+            dispatch({type: 'LOGIN', payload: res.user})
 
             setIsPending(false)
             setError(null)
