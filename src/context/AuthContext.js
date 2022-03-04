e8622d085aaea5a32074c9650e478cd287a29cca
robera16 <roberaworknehrobwork@gmail.com>
2022-03-04 18:40:21 +0300

    wait until auth is ready

--- src/context/AuthContext.js
+++ src/context/AuthContext.js
@@ -1,4 +1,5 @@
-import { createContext, useReducer } from "react";
+import { createContext, useReducer, useEffect } from "react";
+import { projectAuth } from "../firebase/config";
 
 export const AuthContext = createContext()
 
@@ -8,6 +9,8 @@ export const authReducer = (state, action) => {
             return { ...state, user: action.payload }
         case 'LOGOUT':
             return { ...state, user: null }
+        case 'AUTH_IS_READY':
+            return { ...state, user: action.payload, authIsReady: true }
         default:
             return state
     }
@@ -15,8 +18,20 @@ export const authReducer = (state, action) => {
 
 export const AuthContextProvider = ({children}) => {
     const [state, dispatch] = useReducer(authReducer, {
-        user: null
+        user: null,
+        authIsReady: false
     })
+
+    /* 
+     run everytime when page refresh or change in authentication status of user
+    */
+    useEffect(() => { 
+       const unsub = projectAuth.onAuthStateChanged((user) => {
+            dispatch({type: 'AUTH_IS_READY', payload: user})
+            unsub() // no longer fire this func in the future
+        })
+    }, [])
+
     console.log('AuthContext state', state)
 
     return (
